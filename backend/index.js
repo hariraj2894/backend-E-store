c727d57333b76f35156a33b190987f8c5afa14b1
hariraj2894 <harirajusudan@gmail.com>
2025-01-10 17:49:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const paymentRoutes = require('./routes/payment');
 const app = express();
 require('dotenv').config();
 
-app.use(cors({ origin: 'http://localhost:3000' })); 
+app.use(cors({ origin: 'https://frontend-e-store.vercel.app/' })); 
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Serve uploads folder
 
